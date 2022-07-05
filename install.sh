5310abfc4eec33aab725fb3e802075288975e403
Yunzhe Wang <ywang393@jhu.edu>
2022-07-05 15:20:24 -0400

    Update install.sh for a setup.py easy_install error.

--- install.sh
+++ install.sh
@@ -2,6 +2,11 @@
 
 python -m pip install --upgrade --user pip build setuptools
 python -m build
-python -m pip install -e .    # Development install
+python -m pip install --prefix=~/.local -e .
+# The --user option causes a bizzare problem which pertains for more than a year
+# now. Check: https://github.com/pypa/pip/issues/7953
+# A workaround is to use --prefix=~/.local instead of --user.
+# python -m pip install --user -e .    # Development install
+
 # For a deployment installation:
 # python -m pip install --force-reinstall dist/PyMatsci-0.0.1-py3-none-any.whl
