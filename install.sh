def722e37a98c5913118f6b64dfc81c3676c4607
Yunzhe Wang <ywang393@jhu.edu>
2022-07-05 14:44:47 -0400

    Update install to install in development mode.

--- install.sh
+++ install.sh
@@ -2,4 +2,6 @@
 
 python -m pip install --upgrade --user pip build setuptools
 python -m build
-python -m pip install --force-reinstall dist/PyMatsci-0.0.1-py3-none-any.whl
+python -m pip install -e .    # Development install
+# For a deployment installation:
+# python -m pip install --force-reinstall dist/PyMatsci-0.0.1-py3-none-any.whl
